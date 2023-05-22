5abd0099fd8e348fa36d0c7927d8fd37689cde94
ElianArismendi <elian.arismendi@interatica.com>
2023-05-22 18:58:50 -0300

    start C# language revise

---
+++ console/Practice/Practice/ArrayExcercises.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class ArrayExcercises
+    {
+        public void arrayDeclaration()
+        {
+            // Simple arr
+            int[] arr = new int[10]; 
+            int[] arr2 = new int[] {1,2,3,4,5,6,7,8,9,10};
+            string[] arr3 = { "a", "b", "c"};
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i; 
+            }       
+
+            printArr(arr);
+            printArr(arr2);
+            printArr(arr3);
+        }
+
+        public void printArr<T>(T[] arr)
+        {
+            foreach (T element in arr)
+                Console.Write($" - {element} - ");
+            Console.WriteLine("\n");
+        }
+
+        public void maxtrixDeclaration()
+        {
+
+            int[,] matrix = new int[3,3];
+            int n = 1; 
+            // Fill matrix
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i,j] = n;
+                    n++;
+                }
+            }
+            // Print 
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($" {matrix[i, j]} ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        public void vectorDeclaration()
+        {
+            int [][] vector = new int[5][];
+
+            // Create
+            for (int i = 0; i < 5; i++)
+            {
+                vector[i] = new int[i + 1];   
+            }
+
+            // Print 
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = 0; j < vector[i].Length; j++)
+                {
+                    Console.Write(vector[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ console/Practice/Practice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Arrays();
+            // Random();
+            Dates();
+        }
+
+
+        static void Arrays()
+        {
+            ArrayExcercises arrExcercises = new ArrayExcercises();
+
+            // arrExcercises.arrayDeclaration();
+            // arrExcercises.maxtrixDeclaration(); 
+            // arrExcercises.vectorDeclaration();
+        }
+
+        static void Random()
+        {
+            // Print a piramid 
+            int i = 1;
+            while(true)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write(" 0 ");
+                }
+                Console.WriteLine();
+
+                i *= 2;
+
+                if (i > 10) break; 
+            }
+        }
+
+        static void Dates()
+        {
+            Dates datesExcercises = new Dates();
+            datesExcercises.DatesDeclaration();
+        }
+    }
+}
