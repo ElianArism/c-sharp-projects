c834d487368ffbc81786f9e1ff2a2dd1cdd5edb9
ElianArismendi <elian.arismendi@interatica.com>
2023-05-23 17:20:12 -0300

    Implement dates operation methods

--- console/Practice/Practice/Dates.cs
+++ console/Practice/Practice/Dates.cs
@@ -15,13 +15,28 @@ namespace Practice
             DateTime d1 = DateTime.Now;
             DateTime d2 = new DateTime(2022, 10, 5); // Year, month, day
             DateTime d3 = new DateTime(2022, 10, 5, 10, 50, 50); // Year, month, day, hour, min, sec
-            DateTime d4 = new DateTime(2022, 10, 5).AddYears(1).AddMonths(1).AddDays(5).AddHours(2) ;
-
+            DateTime d4 = new DateTime(2022, 10, 5).AddYears(1).AddMonths(1).AddDays(5).AddHours(2);
 
             Console.WriteLine(d1);
             Console.WriteLine(d2);
             Console.WriteLine(d3);
             Console.WriteLine(d4);
+        }
+
+        public void DatesOperation()
+        {
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = new(2023, 05, 22); // Year, month, day
+
+            // Substraction 
+            TimeSpan d3 = d1 - d2; 
+            Console.WriteLine(d1);
+            Console.WriteLine(d2); 
+            Console.WriteLine($" Dates substraction {d3.Days}");
+            
+            // Addition 
+            int totalDays = d1.Day + d2.Day;
+            Console.WriteLine(totalDays);
         }
 
     }
